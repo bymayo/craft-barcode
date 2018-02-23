be270297db88b49f524927d35f0fbf798417f723
Giel Tettelaar <water.giel@gmail.com>
2018-02-23 20:14:39 +0100

    Initial Craft 3 Commit

---
+++ src/translations/en/barcode.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * BarcodeService plugin for Craft CMS 3.x
+ *
+ * BarcodeService is a Craft CMS plugin that allows you to generate a barcode image (Or HTML) in your templates using Twig.
+ *
+ * @link      https://bymayo.co.uk
+ * @copyright Copyright (c) 2018 ByMayo
+ */
+
+/**
+ * BarcodeService en Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('barcode', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    ByMayo
+ * @package   BarcodeService
+ * @since     1.0.0
+ */
+return [
+    'BarcodeService plugin loaded' => 'BarcodeService plugin loaded',
+];
