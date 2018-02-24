7fdb91e20e357c881a6638c2c36d9c204694a0f7
Giel Tettelaar <water.giel@gmail.com>
2018-02-24 10:34:59 +0100

    Remove unecesary comments

--- src/variables/BarcodeVariable.php
+++ src/variables/BarcodeVariable.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * BarcodeService plugin for Craft CMS 3.x
+ * BarcodeVariable for Barcode plugin for Craft CMS 3.x
  *
- * BarcodeService is a Craft CMS plugin that allows you to generate a barcode image (Or HTML) in your templates using Twig.
+ * Barcode is a Craft CMS plugin that allows you to generate a barcode image (Or HTML) in your templates using Twig.
  *
  * @link      https://bymayo.co.uk
  * @copyright Copyright (c) 2018 ByMayo
@@ -14,38 +14,15 @@ use bymayo\barcode\Barcode;
 
 use Craft;
 
-/**
- * BarcodeService Variable
- *
- * Craft allows plugins to provide their own template variables, accessible from
- * the {{ craft }} global variable (e.g. {{ craft.barcode }}).
- *
- * https://craftcms.com/docs/plugins/variables
- *
- * @author    ByMayo
- * @package   BarcodeService
- * @since     1.0.0
- */
 class BarcodeVariable
 {
-    // Public Methods
-    // =========================================================================
 
     /**
-     * Generate a barcode. Use the below twig:
-     *
-     *  {{ craft.barcode.generate(  {
-     *          code: '8745364267452',
-     *          barcodeImageType: 'svg',
-     *          type: 'TYPE_CODE_128'
-     *  } ) }}
-     *
+     * Generate a barcode based on an array of options passed into twig.
      * @param array $optional
      * @return string
      */
-    /*
-	 * Generate BarcodeService Object
-	*/
+
     public function generate(array $options)
     {
         return Barcode::$plugin->barcodeService->generate($options);
